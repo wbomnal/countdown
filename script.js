a4c9207fdfc1daf0fe9a9283e5ac8b6652b21df3
Wilson Bomnal <91630614+wbomnal@users.noreply.github.com>
2022-05-09 09:20:27 +0000

    added a sum element to keep adding all entries

--- script.js
+++ script.js
@@ -4,25 +4,42 @@
 // change the count-el in the HTML to reflect the new count
 
 let count = 0
+// let counts = [count]
+let sumT = count
+
 let countEl = document.getElementById("count-el")
 let infoEl = document.getElementById("info")
+let sumEl = document.getElementById("sum-el")
+
 function increment()
 {
     count = count + 1
-countEl.innerText = count
+countEl.textContent =  count 
 };
 
 function decrement()
 {
     count = count - 1
-countEl.innerText = count
+countEl.textContent = count
 };
+
+function sumTotal(){
+  sumT += count
+ sumEl.textContent = "Sum: " + sumT
+  
+}
 
 function save()
 {
   let countStr = count + " - "
-
-  infoEl.innerText += countStr
-  countEl.innerText = 0
-      count = 0
+        countEl.textContent += count + " "
+    
+  sumTotal()
+  infoEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 };
+
+// function sumTotal(){
+//   sumEl.textContent += countEl.textContext
+// }
